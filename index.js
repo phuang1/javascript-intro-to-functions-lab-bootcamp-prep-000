b89b501d6556a979463da7c5100cde93b0f49659
p huang <huang.pien@gmail.com>
2018-01-08 11:40:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return ("I love you too.")
   } else if (string === "thirsty") {
-    return ("Refilling water bowl.")
-  } else {
-    console.log("Reading newspaper.")
+    return ("I can\'t hear you!")
+  } else if {string === ) {
+    return ("Reading newspaper.")
   }
 }
 it('returns "I can\'t hear you!" if `string` is lowercase', function() {
