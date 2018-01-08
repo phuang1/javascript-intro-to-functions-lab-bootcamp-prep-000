c57558266edc2f67200d194b5c1aaa9d535e68a4
p huang <huang.pien@gmail.com>
2018-01-08 11:49:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ function logWhisper(string) {
 function sayHiToGrandma(string) {
   if (string === "I love you, Grandma.") {
     return ("I love you, too.")
-  } else if (string === `.toLowerCase()`) {
+  } else if (string === {
     return ("I can\'t hear you!")
   } else if (string === `.toUpperCase()`) {
     return ("YES INDEED!")
