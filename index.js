92b4f2134a8191a892684c43a2cb09b82f8b125c
p huang <huang.pien@gmail.com>
2018-01-08 11:50:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ if (string === "I love you, Grandma.") {
     return ("I love you, too.")
   } else if (string === lowercase) {
     return ("I can\'t hear you!")
-  } else if (string === uppercase() {
+  } else if (string === uppercase) {
     return ("YES INDEED!")
   }
 }
