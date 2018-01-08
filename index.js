e993c31c38f49b0a7e199330c21dec9e6a145fba
p huang <huang.pien@gmail.com>
2018-01-08 11:35:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,5 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+    var dog = "thirsty"
+ 
+  if (dog === "hungry") {
+    console.log("Refilling food bowl.")
+  } else if (dog === "thirsty") {
+    console.log("Refilling water bowl.")
+  } else {
+    console.log("Reading newspaper.")
+  }
 }
